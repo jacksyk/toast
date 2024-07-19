5d0d36cb511a3f8dafce43bd4413ba31a2490650
Shu Yi Kang <185901261@qq.com>
2024-07-19 17:25:24 +0800

    feat(master): [build] 开发 && 生产模式区分

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,16 @@
 import { terser } from "rollup-plugin-terser"
 import postcss from "rollup-plugin-postcss"
+import serve from "rollup-plugin-serve"
 
 import typescript from "@rollup/plugin-typescript"
+
+const getEnvironment = () => {
+    const args = process.argv
+    const _ENV_INDEX = args.findIndex((arg) => arg.startsWith("--env"))
+    return args[_ENV_INDEX + 1]
+}
+
+const ENV = getEnvironment()
 
 const config = {
     input: "src/index.ts",
@@ -12,6 +21,11 @@ const config = {
         },
     ],
     plugins: [
+        ENV === "development" &&
+            serve({
+                contentBase: "./",
+                port: 9000,
+            }),
         typescript({
             tsconfig: "./tsconfig.json",
         }),
